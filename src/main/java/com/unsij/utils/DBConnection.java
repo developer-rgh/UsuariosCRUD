0d1ea0ebc8c84ad147f4f9dda8cf9868ab991108
developer-unsij <rubengarcia@unsij.edu.mx>
2025-06-24 10:05:54 -0600

    Ajustes de variables de entorno para produccion

--- src/main/java/com/unsij/utils/DBConnection.java
+++ src/main/java/com/unsij/utils/DBConnection.java
@@ -4,13 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
+
     public static Connection getConnection() {
         try {
+            String url = System.getenv("DB_URL");
+            String user = System.getenv("DB_USER");
+            String pass = System.getenv("DB_PASS");
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/ejemplo_mvc?useSSL=false&serverTimezone=UTC",
-                "root",
-                "toor"
+                    url,
+                    user,
+                    pass
             );
         } catch (Exception e) {
             throw new RuntimeException("Error de conexión", e);
